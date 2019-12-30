684ec9c5cde69cf9090a60c011673b9ec003eac8
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-12-31 00:39:37 +0530

    completed first pass of recodebase

--- ArpAuthenticator.java
+++ ArpAuthenticator.java
@@ -1,12 +1,18 @@
 package net.floodlightcontroller.sdn_arp_spoof_detection;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
+import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
+import org.projectfloodlight.openflow.protocol.OFFlowDelete;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.protocol.OFVersion;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.match.Match;
 import org.projectfloodlight.openflow.protocol.match.MatchField;
 import org.projectfloodlight.openflow.types.EthType;
 import org.projectfloodlight.openflow.types.IPv4Address;
@@ -25,10 +31,8 @@ import net.floodlightcontroller.core.module.IFloodlightService;
 import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.DHCP;
 import net.floodlightcontroller.packet.Ethernet;
-import net.floodlightcontroller.packet.IPv4;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 import net.floodlightcontroller.dhcpserver.*;
 public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
@@ -36,7 +40,7 @@ public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
 	protected IFloodlightProviderService floodlightProviderService;
 	protected HashMap<IOFSwitch,HashMap<OFPort,IPv4Address>> ipPortMap;
 	protected HashMap<IPv4Address,IOFSwitch> ipSwitchMap;
-	protected HashSet<SwitchPortPair> blockedSet;
+	protected HashMap<IOFSwitch,HashMap<OFPort,ArrayList<IPv4Address>>> blockedMap;
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
@@ -54,6 +58,50 @@ public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	private Match createMatch(IOFSwitch sw,OFPort port,IPv4Address addr){
+		OFFactory factory = sw.getOFFactory();
+		return factory.buildMatch().setExact(MatchField.IN_PORT,port)
+			.setExact(MatchField.ETH_TYPE,EthType.ARP)
+			.setExact(MatchField.ARP_SPA,addr)
+			.build();
+	}
+	private void writeBlockFlow(Match m,IOFSwitch sw){
+		OFFactory factory = sw.getOFFactory();
+		ArrayList<OFAction> actions = new ArrayList<>();
+		OFFlowAdd flow =factory.buildFlowAdd().setMatch(m).setHardTimeout(0).setIdleTimeout(0).setActions(actions).build();
+		sw.write(flow);
+	}
+	private void writeUnblockFlow(IOFSwitch sw,Match m){
+		OFFactory factory = sw.getOFFactory();
+		OFFlowDelete flow =factory.buildFlowDelete().setMatch(m).build();
+		sw.write(flow);
+	}
+	private void unblockIfMalicious(IOFSwitch sw,OFPort port){
+		HashMap<OFPort,ArrayList<IPv4Address>> switchMap = blockedMap.get(sw);
+		if(switchMap == null) return;
+		ArrayList<IPv4Address> blockList = switchMap.get(port);
+		if(blockList == null) return;
+		for(IPv4Address addr : blockList){
+			Match m = createMatch(sw,port,addr);
+			writeUnblockFlow(sw,m);
+		}
+		blockList.clear();
+	}
+	private void block(OFPort port,IOFSwitch sw,IPv4Address addr){
+		HashMap<OFPort,ArrayList<IPv4Address>> switchMap = blockedMap.get(sw);
+		if(switchMap == null) {
+			switchMap = new HashMap<>();
+			blockedMap.put(sw,switchMap);
+		}
+		ArrayList<IPv4Address> blockList = switchMap.get(port);
+		if(blockList == null){
+			blockList = new ArrayList<>();
+			switchMap.put(port,blockList);
+		}
+		blockList.add(addr);
+		Match m = createMatch(sw,port,addr);
+		writeBlockFlow(m,sw);
+	}
 	protected Command handlePacketInMessage(IOFSwitch sw,OFPacketIn pi,FloodlightContext cntx){
 		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 		OFPort inPort = (pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort()
@@ -61,7 +109,9 @@ public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
 		HashMap<OFPort,IPv4Address> actMap = ipPortMap.get(sw);
 		if(actMap == null){
 			actMap = new HashMap<>();
-			ipPortMap.put(sw,actMap); } if(eth.getEtherType().equals(EthType.ARP)){
+			ipPortMap.put(sw,actMap); 
+		}
+		if(eth.getEtherType().equals(EthType.ARP)){
 			ARP arp = (ARP) eth.getPayload();
 			IPv4Address addr = arp.getSenderProtocolAddress();
 			IOFSwitch actSwitch = ipSwitchMap.get(addr);
@@ -74,7 +124,7 @@ public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
 				if(actSwitch != sw) return Command.CONTINUE;
 				IPv4Address realAddr = actMap.get(inPort);
 				if(!realAddr.equals(addr)){
-					block(inPort,sw);
+					block(inPort,sw,addr);
 					return Command.STOP;
 				}
 			}
@@ -92,25 +142,6 @@ public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
 				if(!ipSwitchMap.containsKey(addr)){
 					ipSwitchMap.put(addr,sw);
 					actMap.put(inPort,addr);
-				}
-			}else{
-			}
-		}else{
-			if(eth.getEtherType().equals(EthType.IPv4)){
-				IPv4 ip = (IPv4) eth.getPayload();
-				IPv4Address addr = ip.getSourceAddress();
-				IOFSwitch actSwitch = ipSwitchMap.get(addr);
-				if(actSwitch == null){
-					ipSwitchMap.put(addr,sw);
-					actSwitch = sw;
-					actMap.put(inPort,addr);
-				}else{
-					if(actSwitch != sw) return Command.CONTINUE;
-					IPv4Address realAddr = actMap.get(inPort);
-					if(!realAddr.equals(addr)){
-						block(inPort,sw);
-						return Command.STOP;
-					}
 				}
 			}
 		}
@@ -147,7 +178,7 @@ public class ArpAuthenticator implements IFloodlightModule, IOFMessageListener {
 		floodlightProviderService = context.getServiceImpl(IFloodlightProviderService.class);
 		ipPortMap = new HashMap<>();
 		ipSwitchMap = new HashMap<>();
-		blockedSet = new HashSet<>();
+		blockedMap = new HashMap<>();
 	}
 
 	@Override
